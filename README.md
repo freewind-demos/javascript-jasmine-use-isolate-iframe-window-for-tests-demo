d0113ad14e91cbca56be4e8596430ce5f9d2787c
freewind <nowindlee@gmail.com>
2018-09-26 19:20:53 +0800

    demo

--- README.md
+++ README.md
@@ -1,15 +1,13 @@
-JavaScript Jasmine JQuery Tests Demo
-=====================================
+JavaScript Jasmine Use Isolate IFrame Window for Tests Demo
+===========================================================
 
-在浏览器中运行Jasmine测试。如果我们的代码中使用jquery，比如`window.$`，怎么测试？
+如果测试需要对`window/document`进行修改，如何保证它们之间互不影响？
 
-Jasmine的做法是简单直接的，直接在`SpecRunner.html`中增加对JQuery的引用，然后再依次导入源文件和测试文件，最后在浏览器中打开`SpecRunner.html`
-
-不过它带来的问题是源代码和测试代码不能使用模块语句。如果我们要使用，可能借助别的工具（比如browserify/webpack等），先对它们进行处理，再引入到`SpecRunner.html`中。
+我想到的办法是为每个测试创建一个独立的iframe，使用它的window
 
 ```
 npm install
-open SpecRunner.html
+npm run test
 ```
 
 ![demo](./images/demo.jpg)
--- spec/hello-spec.js
+++ spec/hello-spec.js
@@ -1,6 +1,32 @@
+function createIFrame() {
+  $('<iframe>', {
+    src: 'about:blank',
+    id: 'myFrame',
+    frameborder: 1,
+    scrolling: 'no'
+  }).appendTo('body');
+  return $('#myFrame')[0].contentWindow;
+}
+
+function removeMyFrame() {
+  $('#myFrame').remove();
+}
+
 describe('JQuery', function () {
-    it('can be tested in jasmine', function () {
-        const result = hello('<div>JQuery</div>')
-        expect(result).toBe('JQuery')
-    })
-})
+  let frameWindow;
+  beforeEach(() => frameWindow = createIFrame());
+  afterEach(() => removeMyFrame());
+
+  it('should run in new isolated window', function () {
+    expect(frameWindow.myData).toEqual(undefined);
+    frameWindow.myData = 123;
+    expect(frameWindow.myData).toEqual(123);
+  });
+
+  it('should run in new isolated window, too', function () {
+    expect(frameWindow.myData).toEqual(undefined);
+    frameWindow.myData = 456;
+    expect(frameWindow.myData).toEqual(456);
+  });
+
+});
